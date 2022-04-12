359dda8ad9c1085f1d5040cfdc3b2ae7bdd02b7d
Uduak Essien <acushla4real@gmail.com>
2022-04-12 09:57:28 +0100

    Add deploy and run script

---
+++ smart_contracts/scripts/deploy.js
@@ -0,0 +1,27 @@
+
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+
+  console.log("Contract deployed to: ", nftContract.address);
+
+  let txn = await nftContract.makeAnEpicNFT()
+  await txn.wait()
+  console.log("Minted NFT #1")
+
+  txn = await nftContract.makeAnEpicNFT()
+  await txn.wait()
+  console.log("Minted NFT #2")
+}
+
+
+;(async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+})()
