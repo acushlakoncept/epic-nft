c7481922f5776081835adb248e31ece4cd3484e7
Uduak Essien <acushla4real@gmail.com>
2022-04-12 13:35:09 +0100

    Update contract to display random background

--- smart_contracts/contracts/MyEpicNFT.sol
+++ smart_contracts/contracts/MyEpicNFT.sol
@@ -15,9 +15,19 @@ contract MyEpicNFT is ERC721URIStorage {
 
   string baseSvg = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><defs><linearGradient id="a" x1="0%" y1="0%" x2="100%" y2="0%" gradientTransform="rotate(30)"><stop offset="0%" style="stop-color:#6ab1c8;stop-opacity:1"/><stop offset="100%" style="stop-color:#bde9fa;stop-opacity:1"/></linearGradient></defs><rect width="100%" height="100%" fill="url(#a)"/><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="fill:&quot;#032a37&quot;;font-weight:700;font-family:tahoma;font-size:16px">';
 
+  string baseSvgStart = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><defs><linearGradient id="a" x1="0%" y1="0%" x2="100%" y2="0%" gradientTransform="rotate(30)"><stop offset="0%" style="stop-color:';
+  string baseSvgMid1 = ';stop-opacity:1"/><stop offset="100%" style="stop-color:';
+  string baseSvgMid2 = ';stop-opacity:1"/></linearGradient></defs><rect width="100%" height="100%" fill="url(#a)"/><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="fill:&quot;#032a37&quot;;font-weight:700;font-family:tahoma;font-size:16px">';
+  string baseSvgEnd = '</text></svg>';
+
   string[] firstWords = ["Super", "Amazing", "Epic", "Crazy", "Fantastic", "Wild"];
   string[] secondWords = ["Power", "Stunt", "Chicken", "Code", "Game", "Salad"];
   string[] thirdWords = ["Naruto", "Sasuke", "Goku", "Mbre", "Esit", "Mmoyo"];
+
+  string[] colorsFrom = ["#6ab1c8", "#08C2A8", "#60c657", "orange", "blue", "green"];
+  string[] colorsTo = ["#bde9fa", "#08C2A8", "#35aee2", "yellow", "#6ab1c8", "green"];
+
+  event NewEpicNFTMinted(address sneder, uint256 tokenId);
 
   constructor() ERC721 ("AcushlaNFT", "SQUARE") {
     console.log("This is my NFT contract. Whoa!");
@@ -41,6 +51,18 @@ contract MyEpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+  function pickRandomColorFrom(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colorsFrom.length;
+    return colorsFrom[rand];
+  }
+
+  function pickRandomColorTo(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOURS", Strings.toString(tokenId))));
+    rand = rand % colorsTo.length;
+    return colorsTo[rand];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
     return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -54,7 +76,10 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory thirdWord = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(firstWord, secondWord, thirdWord));
 
-    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+    string memory randomColorFrom = pickRandomColorFrom(newItemId);
+    string memory randomColorTo = pickRandomColorTo(newItemId);
+
+    string memory finalSvg = string(abi.encodePacked(baseSvgStart, randomColorFrom, baseSvgMid1, randomColorTo, baseSvgMid2, combinedWord, baseSvgEnd));
 
     string memory json = Base64.encode(
       bytes(
@@ -92,6 +117,8 @@ contract MyEpicNFT is ERC721URIStorage {
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     _tokenIds.increment();
+
+    NewEpicNFTMinted(msg.sender, newItemId);
 
   }
 }
